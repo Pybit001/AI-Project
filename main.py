8fb9f84978e05645d573d9037bccfb2c2974b2d7
MarkPrimak <>
2021-07-27 20:20:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+### Hello ###
+
+### Name of AI is under development ###
+
+### AI itself is under development ###
+
+
+# There are resources for voice recognition and speech #
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak():
+  engine.say("abc")
+  engine.runAndWait()
+speak()
+
+
+
